3ef31c084214e0bf9fa8bcf0ad33bf3eaba24bda
Khellermann Machado <khellermann@gmail.com>
2019-09-13 15:27:22 -0300

    Configurações Schema para validação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,17 @@
 import User from '../models/User';
+import * as Yup from 'yup';
 class UserController{
     async store(req, res){
+
+        const schema = Yup.object().shape({
+            name:Yup.string().required(),
+            email:Yup.string().email().required(),
+            password:Yup.string().required().min(6) 
+        });
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(401).json({error: "Favor conferir as informações"});            
+        }
         const user = await User.findOne({where: {email:req.body.email}});
         if(user){
             return res.status(401).json({error: "Usuário já cadastrado"});
