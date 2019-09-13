bcc72bb19671ee1454cf4345d029f96c6d30fc7b
Khellermann Machado <khellermann@gmail.com>
2019-09-13 14:02:38 -0300

    Configuração Sequelize/Migrations

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "machado",
+    database: "meetapp",
+    define:{
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+}
