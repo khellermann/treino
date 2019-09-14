c8bd3e0eb6868d0a04dafbdddcd5a5691d2f86ac
Khellermann Machado <khellermann@gmail.com>
2019-09-14 11:18:45 -0300

    Trabalhando com uploads

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,29 +1,36 @@
-import Sequelize, {Model} from 'sequelize'
-import bcryptjs from 'bcryptjs';
+import Sequelize, { Model } from "sequelize";
+import bcryptjs from "bcryptjs";
 class User extends Model {
-    static init(sequelize){
-        super.init({
-            name: Sequelize.STRING,
-            email: Sequelize.STRING,
-            password_hash: Sequelize.STRING,
-            password: Sequelize.VIRTUAL,
-            provider: Sequelize.BOOLEAN
-        },{
-            sequelize
-        })
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        provider: Sequelize.BOOLEAN
+      },
+      {
+        sequelize
+      }
+    );
 
-        this.addHook("beforeSave", async user=>{
-            user.password_hash = await bcryptjs.hash(user.password, 8); 
-        });
+    this.addHook("beforeSave", async user => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: "avatar_id" });
+  }
+
+  checkPassword(password) {
+    if (password) {
+      return bcryptjs.compare(password, this.password_hash);
     }
-
-    checkPassword(password){
-        if(password){
-            return bcryptjs.compare(password, this.password_hash);
-        }
-    }
-
-
+  }
 }
 
 export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
-import {Router} from 'express';
+import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
-
-import AuthMiddlewares from './app/middlewares/auth';
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FIleController";
+import AuthMiddlewares from "./app/middlewares/auth";
 const routes = new Router();
+const uploads = multer(multerConfig);
 
 routes.post("/user", UserController.store);
 routes.post("/session", SessionController.store);
@@ -12,4 +15,6 @@ routes.post("/session", SessionController.store);
 routes.use(AuthMiddlewares);
 
 routes.put("/user", UserController.update);
+
+routes.post("/files", uploads.single("file"), FileController.store);
 export default routes;
